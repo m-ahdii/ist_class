c6417237941929f271f937d685c1cd0d30ed1e38
m-ahdii <mmmoradkhani2003@gmail.com>
2025-06-12 08:05:29 +0300

    Initial commit with two Python files

---
+++ eg_4.py
@@ -0,0 +1,5 @@
+number = -1
+
+while number < 0:
+    number = int(input("Enter a positive number: "))
+    print("You entered:", number)
---
+++ eg_5.py
@@ -0,0 +1,12 @@
+while True:
+    print("Menu:")
+    print("1. Say helloo")
+    print("2. Exit")
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        print("Hello")
+    elif choice == "2":
+        print("Goodbye!")
+    else:
+        print("invalid choice, try again.")
